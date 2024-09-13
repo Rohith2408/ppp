9e9f377ec0f887b60f38f55e3123b0bb3039462d
Rohith2408 <kumarrohith24081999@gmail.com>
2024-09-14 04:43:40 +0530

    tb to tba

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -22,7 +22,7 @@ const Section1=()=>{
     ]).current;
     const [currentbg,setCurrentBg]=useState(0)
     const interval=useRef<any>()
-    const ca=useRef("TB").current
+    const ca=useRef("TBA").current
 
     useEffect(()=>{
 
